b4445254a9a98300a0b9af1f5ea185acc2422039
wilderxD <wilder1093@gmail.com>
2026-09-22 11:25:49 -0500

    fix: point API to Oracle Cloud (144.22.246.70)

--- src/environments.ts
+++ src/environments.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  apiUrl: 'https://script.google.com/macros/s/AKfycbwAPa5kbCNTnNd5M3CeWt_VB_YClzkNT6N5-A4pSu-xUnc-Fb7xHFPrz4LLk2qUBgL6Bw/exec',
+  apiUrl: 'http://144.22.246.70/api',
   apiToken: 'forli-9f2b7c41e8d3-2026-xtR7mK2q',
 }
